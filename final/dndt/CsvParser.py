a54c49590af635e8bdbb9ae763e0f10c62fe71e4
Pietro Caragiulo <31522010+pietro-caragiulo@users.noreply.github.com>
2019-12-08 13:20:47 -0800

    after tabnet run

--- final/dndt/CsvParser.py
+++ final/dndt/CsvParser.py
@@ -96,8 +96,8 @@ class CsvParser(object):
 
     def filter(self, df):
         # list of columns/features in the output dataframe
-        column_select = ['vzPull', 'vy', 'vz', 'uncM', 'eleZ0', 'posZ0','projY']  # From Matt
-        # column_select = ['vz','vzPull','vy','vyPull','uncM','eleZ0','posZ0','projY','eleTrkTanLambda','posTrkTanLambda']
+        # column_select = ['vzPull', 'vy', 'vz', 'uncM', 'eleZ0', 'posZ0','projY']  # From Matt
+        column_select = ['vz','vzPull','vy','vyPull','uncM','eleZ0','posZ0','projY','eleTrkTanLambda','posTrkTanLambda']
 
 
         self.DATA_HEADER = column_select
